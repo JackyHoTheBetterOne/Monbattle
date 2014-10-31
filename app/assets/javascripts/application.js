70b11e456484cac7f5890fd1bd04f768a3e86a11
Scrulox <hk.ling09@yahoo.com>
2014-10-31 12:20:25 -0700

    the admin tool is

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,39 +14,8 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
-//= require_tree .
 //= require jquery-ui
-
-// window.fbAsyncInit = function() {
-//   FB.init({
-//     appId      : '464134073726426',
-//     xfbml      : true,
-//     version    : 'v2.1'
-//   });
-
-//   function onLogin(response) {
-//     if (response.status == 'connected') {
-//       FB.api('/me?fields=first_name', function(data) {
-//         var welcomeBlock = document.getElementById('fb-welcome');
-//         welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
-//       });
-//     }
-//   };
-
-//   FB.getLoginStatus(function(response) {
-//     // Check login status on load, and if the user is
-//     // already logged in, go directly to the welcome message.
-//     if (response.status == 'connected') {
-//       onLogin(response);
-//     } else {
-//       // Otherwise, show Login dialog first.
-//       FB.login(function(response) {
-//         onLogin(response);
-//       }, {scope: 'user_friends, email'});
-//     }
-//   });
-
-// };
+//= require_tree .
 
 
 window.fbAsyncInit = function() {
@@ -57,11 +26,4 @@ window.fbAsyncInit = function() {
   });
 };
 
-// (function(d, s, id){
-//    var js, fjs = d.getElementsByTagName(s)[0];
-//    if (d.getElementById(id)) {return;}
-//    js = d.createElement(s); js.id = id;
-//    js.src = "//connect.facebook.net/en_US/sdk.js";
-//    fjs.parentNode.insertBefore(js, fjs);
-//  }(document, 'script', 'facebook-jssdk'));
 
