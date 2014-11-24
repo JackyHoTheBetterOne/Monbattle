37dbf1df35d613c2c745ba12494bb1d61c81fcb7
Alan Maule <amaule77@gmail.com>
2014-11-23 16:35:14 -0800

    saving updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,23 @@
 class HomeController < ApplicationController
   layout "facebook_landing"
+  before_action :find_user, :find_ability_purchases, only: [:index, :abilities_for_mon]
 
   def index
-    @user      = params[:user] || current_user
-    @base_mons = MonsterUnlock.base_mons(@user)
-    @abilities = Ability.includes(:ability_purchases).includes(:abil_socket).includes(:jobs).abilities_purchased(@user)
-    @ability_purchases = @user.ability_purchases
+    @monster_unlocks = @user.monster_unlocks
+    @base_mons = @monster_unlocks.base_mons(@user)
     @members = @user.parties.first.members
+    @abilities = Ability.includes(:ability_purchases).includes(:abil_socket).includes(:jobs).abilities_purchased(@user)
     unless @user
       render layout: "facebook_landing"
     end
+  end
+
+  def abilities_for_mon
+    @mon = MonsterUnlock.find params[:mon]
+    @socket = params[:socket]
+    # @abilities = Ability.includes(:ability_purchases).includes(:abil_socket).includes(:jobs).abilities_purchased(@user)
+    @abilities = Ability.find_default_abilities_available(@socket, @mon.job).abilities_purchased(@user)
+    render :abilities_for_mon
   end
 
   def store
@@ -60,6 +68,16 @@ class HomeController < ApplicationController
       flash[:alert] = "Something went wrong. It's your fault."
       redirect_to device_store_path
     end
+  end
+
+  private
+
+  def find_user
+    @user = current_user
+  end
+
+  def find_ability_purchases
+    @ability_purchases = @user.ability_purchases
   end
 
 end
