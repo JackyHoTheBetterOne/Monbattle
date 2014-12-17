c149904987c37e2aad431f1ea25adeb11b0de356
Frank Yan <frank.yu.yan@gmail.com>
2014-12-17 12:48:30 -0800

    hi

--- db/schema.rb
+++ db/schema.rb
@@ -123,7 +123,7 @@ ActiveRecord::Schema.define(version: 20141215040918) do
     t.text     "description"
     t.text     "victory_message"
     t.text     "ability_reward",          default: [], array: true
-    t.integer  "stamina_cost"
+    t.integer  "stamina_cost",            default: 0
   end
 
   add_index "battle_levels", ["area_id"], name: "index_battle_levels_on_area_id", using: :btree
@@ -437,8 +437,8 @@ ActiveRecord::Schema.define(version: 20141215040918) do
     t.boolean  "is_active",          default: true
     t.string   "bonus"
     t.integer  "reward_amount"
-    t.datetime "end_date",           default: '2015-11-20 20:00:42'
-    t.datetime "refresh_date",       default: '2015-11-20 20:00:42'
+    t.datetime "end_date",           default: '2015-11-24 06:45:56'
+    t.datetime "refresh_date",       default: '2015-11-24 06:45:56'
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "quest_type_id"
@@ -526,7 +526,7 @@ ActiveRecord::Schema.define(version: 20141215040918) do
     t.integer  "stamina",                      default: 100
     t.integer  "level",                        default: 1
     t.integer  "seconds_left_for_next_energy"
-    t.datetime "last_update_for_energy",       default: '2014-12-08 22:10:30'
+    t.datetime "last_update_for_energy",       default: '2014-12-09 21:13:37'
     t.text     "completed_areas",              default: [],                    array: true
     t.text     "completed_regions",            default: [],                    array: true
     t.string   "recently_unlocked_level",      default: ""
