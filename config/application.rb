6fbf8f2fba99c8c643b12bdd0453aa2d81c8a7bb
Scrulox <hk.ling09@yahoo.com>
2014-12-29 22:49:52 -0800

    one last trail

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ Bundler.require(*Rails.groups)
 module Monbattle
   class Application < Rails::Application
     config.time_zone = 'Pacific Time (US & Canada)'
+    config.active_record.default_timezone = 'Pacific Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
