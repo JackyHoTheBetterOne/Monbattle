48449195f0cfe1473bc41c2d241487c40ff482a1
Scrulox <hk.ling09@yahoo.com>
2015-02-24 21:33:59 -0800

    A bug

--- app/services/battle/victory.rb
+++ app/services/battle/victory.rb
@@ -67,7 +67,7 @@ class Battle::Victory
       end
     elsif summoner.beaten_levels.include?(battle_level.name)  
       if battle_level.time_requirement
-        if round_taken < battle_level.time_requirement.to_i
+        if round_taken.to_i < battle_level.time_requirement.to_i
           self.reward = "x" + " " + battle_level.second_clear_reward.to_s
           self.reward_image = battle_level.second_clear_reward_image
         end
