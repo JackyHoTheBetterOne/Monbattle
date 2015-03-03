00bf428eb7611c603704f8e513ffb197c20db349
Scrulox <hk.ling09@yahoo.com>
2015-03-02 19:09:58 -0800

    some changed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'],
-           :display => 'page'
+           :display => 'popup'
 end
