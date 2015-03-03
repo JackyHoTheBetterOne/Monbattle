2a1d281ce254281df082794517c1279ea2be82e9
Scrulox <hk.ling09@yahoo.com>
2015-03-02 21:50:27 -0800

    one last trial

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'],
-           :display => 'page'
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'],
+#            :display => 'page'
+# end
