f36cfc5c588ffad708b45020b8a6faabb2873a01
Scrulox <hk.ling09@yahoo.com>
2015-03-20 16:18:08 -0700

    fixed some facebook issue

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -235,7 +235,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV["FACEBOOK_ID"], ENV["FACEBOOK_SECRET"]
+  config.omniauth :facebook, ENV["FACEBOOK_ID"], ENV["FACEBOOK_SECRET"], :iframe => true, 
+    :scope => ''
 
 
   # ==> Warden configuration
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@
 #   provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'],
 #            :display => 'page'
 # end
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'], :setup => true
-end
+
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'], :setup => true
+# end
