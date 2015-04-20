fe073359633415f40e4ebcf4abb484d2017a3c78
Scrulox <hk.ling09@yahoo.com>
2015-04-20 13:42:46 -0700

    some bugs

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,5 +1,5 @@
 class ScoresController < ApplicationController
-  def index
-    params[:level_id] params[:guild_id] params[:summoner_id]
-  end
+  # def index
+  #   params[:level_id] params[:guild_id] params[:summoner_id]
+  # end
 end
